15ef94eb39018f5c899074600200bf4c64bda6f9
krosh <krosh@kroshhaker.dev>
2026-08-08 13:05:21 +0300

    feat(vite conf): allow origin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+  return {
+    plugins: [vue()],
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      allowedHosts: [env.DEV_DOMAIN],
+    },
+  }
 })
